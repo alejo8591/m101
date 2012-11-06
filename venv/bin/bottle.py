9c69ed79b3ee6ab71992785b54b2e60d132e253f
Alejandro Romero <alejo8591@gmail.com>
2012-11-06 15:40:10 -0500

    update files

--- venv/bin/bottle.py
+++
@@ -1,5 +0,0 @@
-#!/Users/alejo8591/Documents/m101/venv/bin/python
-# EASY-INSTALL-SCRIPT: 'bottle==0.11.3','bottle.py'
-__requires__ = 'bottle==0.11.3'
-import pkg_resources
-pkg_resources.run_script('bottle==0.11.3', 'bottle.py')
