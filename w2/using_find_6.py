9bb7fbb2e6a955f8c6d0c21693e2bcb3d96aae3f
Alejandro Romero <alejo8591@gmail.com>
2012-11-06 22:43:16 -0500

    other using find

---
+++ w2/using_find_6.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import pymongo
+import sys
+
+connect = pymongo.Connection("mongodb://127.0.0.1", safe=True)
+
+db = connect.reddit
+stories = db.stories
+
+def find():
+    print "Find, reporting for duty"
+    query = {}
+    try:
+        cursor = stories.find(query).sort('student_id', pymongo.ASCENDING).skip(4).limit(1)
+    except:
+        print "Unexpected error:",sys.exc_info()[0]
+        
+    sanity = 0
+    for doc in cursor:
+        print doc
+        sanity+=1
+        if (sanity > 10):
+            break
+        
+def find_one():
+    print "find one, reporting for duty"
+    query = {'student_id':10}
+    try:
+        iter = scores.find_one(query)
+    except:
+        print "Unexpected error:",sys.exc_info()[0]
+    
+    print iter
+    
+#find_one()
+find()
