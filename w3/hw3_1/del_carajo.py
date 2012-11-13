3e8894141b36fb3d06cadc68798e7695da7c4128
Alejandro Romero <alejo8591@gmail.com>
2012-11-13 13:42:03 -0500

    update algorithm

--- w3/hw3_1/del_carajo.py
+++ w3/hw3_1/del_carajo.py
@@ -5,26 +5,30 @@ connect = pymongo.Connection("mongodb://127.0.0.1", safe=True)
 db = connect.school
 students = db.students
 print students.count()
-i = 0
-for i in range(students.count()+1):
-    homework,position,value=0,0,0
-    homeworks =  students.find({'_id': i}, {"scores":{"$slice":[2,4]}})
-    for homework in range(len(homeworks[0]['scores'])):
-        if value == 0 and value < homeworks[0]['scores'][i]['score']:
-            value = homeworks[0]['scores'][i]['score']
-            position = homework
-        if value > homeworks[0]['scores'][i]['score']:
-            value = homeworks[0]['scores'][i]['score']
-            position = homework
-    students.update({"_id":i}, {"$pop":{"scores":position}})
-        
+try:
+    i = 0
+    for i in range(students.count()+1):
+        homework,position,value=0,0,0
+        homeworks =  students.find({'_id': i}, {"scores":{"$slice":[2,4]}})
+        for homework in range(len(homeworks[0]['scores'])):
+            if value == 0 and value < homeworks[0]['scores'][i]['score']:
+                value = homeworks[0]['scores'][i]['score']
+                position = homework
+            if value > homeworks[0]['scores'][i]['score']:
+                value = homeworks[0]['scores'][i]['score']
+                position = homework
+        students.update({"_id":i}, {"$pop":{"scores":position}})
+except:
+    print "Unexpected error:",sys.exc_info()[0]
 print students.count()
 """
 h,i= 0,0
+homeworks =  students.find({'_id': i})
 for i in range(len(homeworks[0]['scores'])):
      if homeworks[0]['scores'][i]['type'] == 'homework':
          if h == 0 and h < homeworks[0]['scores'][i]['score']: h = homeworks[0]['scores'][i]['score']
          while h > homeworks[0]['scores'][i]['score']:
              h = homeworks[0]['scores'][i]['score']
+print h
 """
 #delete element db.students.update({"_id":0}, {"$pop": {"scores":number element}})
