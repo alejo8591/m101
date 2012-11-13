42dcbb194572abb3b03223315567fe9cbee23091
Alejandro Romero <alejo8591@gmail.com>
2012-11-13 17:13:11 -0500

    update with optimized code

--- w3/hw3_1/del_carajo.py
+++ w3/hw3_1/del_carajo.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import pymongo, sys
-connect = pymongo.Connection("mongodb://127.0.0.1", safe=True)
-db = connect.school
-students = db.students
+students = pymongo.Connection("mongodb://127.0.0.1", safe=True).school.students
 try:
     i = 0
     for i in range(students.count()):
@@ -10,6 +8,6 @@ try:
         homeworks =  students.find({'_id': i})
         for homework in range(len(homeworks[0]['scores'])):
             array.append(homeworks[0]['scores'][homework])
-        array.remove(min(array[2:4])), students.update({'_id':i}, {'$unset':{'scores':1}}), students.update({'_id':i}, {'$set':{'scores':array}})
+        array.remove(min(array[2:4])), students.update({'_id':i}, {'$set':{'scores':array}})
 except:
     print "Unexpected error:",sys.exc_info()[0]
